a7e448e085aa85149f7330bc85e94ce83205bc7f
fukazawa_r <fukazawa_r@thinkone.jp>
2023-11-09 14:51:13 +0900

    react

---
+++ src/components/MyReactComponent.jsx
@@ -0,0 +1,12 @@
+import "./MyReactComponent.css";
+
+const MyReactComponent = () => {
+  return (
+    <div className="my-react-component">
+      <h3>MyReactComponent</h3>
+      <p>This is a simple React component with added style.</p>
+    </div>
+  );
+};
+
+export default MyReactComponent;
