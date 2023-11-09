a9fb2c62ecc4c69de4e556da2651e5990e9cd9df
fukazawa_r <fukazawa_r@thinkone.jp>
2023-11-09 15:00:25 +0900

    vue

---
+++ src/components/MyVueComponent.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="my-vue-component">
+    <h3>MyVueComponent</h3>
+    <p>This is a simple Vue.js component.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MyVueComponent',
+};
+</script>
+
+<style scoped>
+.my-vue-component {
+  background-color: #e0e0e0;
+  padding: 20px;
+  border-radius: 5px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+}
+
+.my-vue-component h3 {
+  color: #333;
+}
+
+.my-vue-component p {
+  color: #666;
+}
+</style>
